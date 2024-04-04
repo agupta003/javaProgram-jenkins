08526cc07fac0e0308f2a9388afed5ffd25e6c5b
agupta003 <vision.abhishek@gmail.com>
2024-04-04 01:39:19 -0400

    Initial commit

---
+++ JenkinTestProj/src/com/jenkins/test/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.jenkins.test;
+
+import java.util.*;
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is Jenkin Test");
+		System.out.println(new Date());
+	}
+
+}
